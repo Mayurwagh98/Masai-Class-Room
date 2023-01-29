0e57e73d34ebff33d2b605178a59f989094aaa02
Hashmat-Noorani <hashmatwani@icloud.com>
2023-01-29 22:18:44 +0530

    added: basic structure

--- my-class-room/src/App.js
+++ my-class-room/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Dashboard from "./screens/Dashboard";
+import Navbar from "./components/Navbar";
+import Class from "./screens/Class";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/dashboard">
+            <Navbar />
+            <Dashboard />
+          </Route>
+          <Route exact path="/class/:id">
+            <Navbar />
+            <Class />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
